e1ca2350d7114d77da5d7a721d979ba698ab2046
Lukehenson89 <lukehenson89@gmail.com>
2025-10-16 14:50:50 +0400

    Add modal functionality to contact form

---
+++ script.js
@@ -0,0 +1,18 @@
+// Modal Script
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("contactBtn");
+const span = document.getElementsByClassName("closeBtn")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
